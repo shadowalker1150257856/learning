33d2c74d06c2185e3f34d268e8b0dc317bd569dd
shadowalker1990 <yuelei1150257856@163.com>
2019-06-15 10:06:48 +0800

    代码格式化

--- learning/learning-java/src/main/java/shadow/leilei/learning/xml/dom/DOMParser.java
+++ learning/learning-java/src/main/java/shadow/leilei/learning/xml/dom/DOMParser.java
@@ -19,14 +19,15 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class DOMParser {
-	
+
 	/**
 	 * 节点类型枚举
+	 * 
 	 * @author Grandata
 	 *
 	 */
-	private static enum NodeTypeEnum{
-		
+	private static enum NodeTypeEnum {
+
 		ELEMENT_NODE(1, "ELEMENT_NODE"), //
 		ATTRIBUTE_NODE(2, "ATTRIBUTE_NODE"), //
 		TEXT_NODE(3, "TEXT_NODE"), //
@@ -39,82 +40,85 @@ public class DOMParser {
 		DOCUMENT_TYPE_NODE(10, "DOCUMENT_TYPE_NODE"), //
 		DOCUMENT_FRAGMENT_NODE(11, "DOCUMENT_FRAGMENT_NODE"), //
 		NOTATION_NODE(12, "NOTATION_NODE");
-		
+
 		private short nodeType;
 		private String nodeTypeName;
-		
-		NodeTypeEnum(int nodeType, String nodeTypeName){
+
+		NodeTypeEnum(int nodeType, String nodeTypeName) {
 			this.nodeType = (short) nodeType;
 			this.nodeTypeName = nodeTypeName;
 		}
-		
-		public static String getNodeTypeName(short nodeType){
-			
-			for(NodeTypeEnum value : values()){
-				if(value.nodeType == nodeType){
+
+		public static String getNodeTypeName(short nodeType) {
+
+			for (NodeTypeEnum value : values()) {
+				if (value.nodeType == nodeType) {
 					return value.nodeTypeName;
 				}
 			}
-			
+
 			return "";
 		}
 	}
-	
+
 	/**
 	 * 打印节点信息
+	 * 
 	 * @param element
 	 */
-	private static void printElementInfo(Element element){
+	private static void printElementInfo(Element element) {
 		// 节点名称, 属性， 及值
-		if(element == null){
+		if (element == null) {
 			return;
 		}
-		
+
 		String nodeTypeName = NodeTypeEnum.getNodeTypeName(element.getNodeType());
 		System.out.println("Element name: " + element.getNodeName() + " type: " + nodeTypeName);
-		
+
 	}
-	
+
 	/**
 	 * 打印节点信息, 如果是text节点则打印内容 <br/>
 	 * Node.getTextContent()方法可以返回节点的所有文本标签信息, 相应的注释和子标签等将被丢弃。<br/>
+	 * 
 	 * @param node
 	 */
-	private static void printNodeInfo(Node node){
+	private static void printNodeInfo(Node node) {
 		// 解析该节点的名称, 属性
 		System.out.print("Node name: " + node.getNodeName());
 		String nodeTypeName = NodeTypeEnum.getNodeTypeName(node.getNodeType());
 		System.out.println(", Node type:  " + nodeTypeName);
-		
+
 		// 打印节点属性
 		printNodeAttributes(node.getAttributes());
-		
+
 		// 解析该节点的子节点
 		NodeList childrenNodeList = node.getChildNodes();
-		if(childrenNodeList != null && childrenNodeList.getLength() > 0){
-		
-			for(int index=0, length=childrenNodeList.getLength(); index<length;index++){
+		if (childrenNodeList != null && childrenNodeList.getLength() > 0) {
+
+			for (int index = 0, length = childrenNodeList.getLength(); index < length; index++) {
 				Node childNode = childrenNodeList.item(index);
 				printNodeInfo(childNode);
 			}
-			
-		}else if(NodeTypeEnum.TEXT_NODE.nodeType == node.getNodeType()){
+
+		} else if (NodeTypeEnum.TEXT_NODE.nodeType == node.getNodeType()) {
 			System.out.println("Node value: " + node.getNodeValue());
-		}else if(NodeTypeEnum.COMMENT_NODE.nodeType == node.getNodeType()){
+		} else if (NodeTypeEnum.COMMENT_NODE.nodeType == node.getNodeType()) {
 			System.out.println("Node value: " + node.getNodeValue());
-		}else if(NodeTypeEnum.CDATA_SECTION_NODE.nodeType == node.getNodeType()){
+		} else if (NodeTypeEnum.CDATA_SECTION_NODE.nodeType == node.getNodeType()) {
 			System.out.println("Node value: " + node.getNodeValue());
 		}
 	}
-	
+
 	/**
 	 * 打印节点属性信息
+	 * 
 	 * @param node
 	 */
-	private static void printNodeAttributes(NamedNodeMap namedNodeMap){
-		if(namedNodeMap != null){
+	private static void printNodeAttributes(NamedNodeMap namedNodeMap) {
+		if (namedNodeMap != null) {
 			System.out.println("Node Attributes:");
-			for(int index=0, length=namedNodeMap.getLength(); index<length; index++){
+			for (int index = 0, length = namedNodeMap.getLength(); index < length; index++) {
 				Node attributeNode = namedNodeMap.item(index);
 				System.out.println("Name: " + attributeNode.getNodeName() + ", Value: " + attributeNode.getNodeValue());
 			}
@@ -122,16 +126,18 @@ public class DOMParser {
 	}
 
 	/**
-	 * 练习使用DOM方式解析xml文件。
-	 * 解析文档，输出每个标签名称， 以及每个标签的属性，
-	 * 如果该标签下没有子标签， 则直接输出该标签的值
+	 * 练习使用DOM方式解析xml文件。</br>
+	 * 解析文档，输出每个标签名称， 以及每个标签的属性， </br>
+	 * 如果该标签下没有子标签， 则直接输出该标签的值 </br>
+	 * DOM解析方式, 语言自带类库, 基于w3c的标准解析
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		String xmlFilePath = "E:\\Github\\shadowalker\\learning\\learning\\learning-java\\src\\main\\java\\shadow\\leilei\\learning\\xml\\XMLTest.xml";
 		File xmlFile = new File(xmlFilePath);
-		
+
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder db = dbFactory.newDocumentBuilder();
@@ -144,7 +150,7 @@ public class DOMParser {
 			System.out.println("DocumentURI: " + document.getDocumentURI());
 			DOMConfiguration domConfig = document.getDomConfig();
 			DOMStringList domStringList = domConfig.getParameterNames();
-			for(int index = 0, length=domStringList.getLength(); index<length; index++){
+			for (int index = 0, length = domStringList.getLength(); index < length; index++) {
 				String domString = domStringList.item(index);
 				System.out.println("DOMStringList DOMString: " + domString);
 			}
@@ -157,39 +163,40 @@ public class DOMParser {
 			System.out.println("XmlStandalone: " + document.getXmlStandalone());
 			Document ownerDocument = document.getOwnerDocument();
 			System.out.println("XmlVersion: " + document.getXmlVersion());
-			
+
 			// 解析xml文档
 			System.out.println("document 属性:");
 			NamedNodeMap namedNodeMap = document.getAttributes();
-			if(namedNodeMap != null) {
-				for(int index=0, length = namedNodeMap.getLength();index<length; index++){
+			if (namedNodeMap != null) {
+				for (int index = 0, length = namedNodeMap.getLength(); index < length; index++) {
 					Node node = namedNodeMap.item(index);
-					System.out.println("Node: " + node.getNodeType() + ", " + node.getNodeName() + ", " + node.getNodeValue());
+					System.out.println(
+							"Node: " + node.getNodeType() + ", " + node.getNodeName() + ", " + node.getNodeValue());
 				}
 			}
-			
+
 			System.out.println("文档第一个节点:");
 			Node firstNode = document.getFirstChild();
 			printNodeInfo(firstNode);
-			
+
 			System.out.println("文档最后一个节点:");
 			printNodeInfo(document.getLastChild());
-			
+
 			System.out.println("解析文档结果:");
 			NodeList nodeList = document.getChildNodes();
-			if(nodeList != null) {
-				for(int index=0, length=nodeList.getLength(); index<length; index++) {
+			if (nodeList != null) {
+				for (int index = 0, length = nodeList.getLength(); index < length; index++) {
 					Node node = nodeList.item(index);
 					printNodeInfo(node);
 				}
 			}
-			
+
 			System.out.println("读取文档标签");
 			Element element = document.getElementById("selectByPrimaryKey");
-			if(element != null){
+			if (element != null) {
 				printElementInfo(element);
 			}
-			
+
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 		} catch (SAXException e) {
@@ -197,7 +204,7 @@ public class DOMParser {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- learning/learning-java/src/main/java/shadow/leilei/learning/xml/jdom/JDOMParser.java
+++ learning/learning-java/src/main/java/shadow/leilei/learning/xml/jdom/JDOMParser.java
@@ -14,34 +14,34 @@ import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
 public class JDOMParser {
-	
+
 	/**
 	 * 解析节点信息
+	 * 
 	 * @param element
 	 */
-	private static void printElementInfo(Element element){
-		if(element == null)
-			return ;
-		
+	private static void printElementInfo(Element element) {
+		if (element == null)
+			return;
+
 		String elementName = element.getName();
 		// 会获取该节点所有文本节点信息值
 		String elementValue = element.getValue();
 		String textContent = element.getText();
 		System.out.println(String.format("打印节点信息, 名称:%s, 值:%s", elementName, elementValue));
 		System.out.println(String.format("节点文本信息: %s", textContent));
-		
+
 		List<Attribute> attributes = element.getAttributes();
-		if(CollectionUtils.isNotEmpty(attributes)){
-			for(Attribute attribute : attributes){
-				System.out.println(String.format("节点属性, name:%s, value:%s", 
-						attribute.getName(), attribute.getValue()));
+		if (CollectionUtils.isNotEmpty(attributes)) {
+			for (Attribute attribute : attributes) {
+				System.out.println(String.format("节点属性, name:%s, value:%s", attribute.getName(), attribute.getValue()));
 			}
 		}
-		
+
 		// content类似于子节点，会将子节点根绝类型构造成content对象
 		List<Content> contents = element.getContent();
-		if(CollectionUtils.isNotEmpty(contents)){
-			for(Content content :contents){
+		if (CollectionUtils.isNotEmpty(contents)) {
+			for (Content content : contents) {
 				CType ctype = content.getCType();
 				System.out.println(String.format("节点ctype: %s, 节点content: %s", ctype.name(), content.getValue()));
 			}
@@ -49,13 +49,14 @@ public class JDOMParser {
 	}
 
 	/**
-	 * JDOM解析xml示例 <br/>
-	 * 解析节点标签的名称, 属性, 值 <br/>
-	 * */
+	 * JDOM解析xml示例 </br>
+	 * 解析节点标签的名称, 属性, 值 </br>
+	 * JDOM的解析依赖于语言特性, 据说性能上也有问题。</br>
+	 */
 	public static void main(String[] args) {
 
 		String filePath = "E:\\Github\\shadowalker\\learning\\learning\\learning-java\\src\\main\\java\\shadow\\leilei\\learning\\xml\\XMLTest.xml";
-		
+
 		SAXBuilder saxBuilder = new SAXBuilder();
 		File file = new File(filePath);
 		try {
@@ -63,18 +64,18 @@ public class JDOMParser {
 			Element rootElement = document.getRootElement();
 			System.out.println("开始解析文档:");
 			printElementInfo(rootElement);
-			
+
 			List<Element> elements = rootElement.getChildren();
-			if(CollectionUtils.isNotEmpty(elements)){
-				for(Element element : elements){
+			if (CollectionUtils.isNotEmpty(elements)) {
+				for (Element element : elements) {
 					printElementInfo(element);
 				}
 			}
-			
+
 		} catch (JDOMException | IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
